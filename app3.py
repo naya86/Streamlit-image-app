a3b758a560667f9f8f4cc10ac854605f816b1847
naya86 <80315799+naya86@users.noreply.github.com>
2021-03-10 10:09:33 +0900

    aa

--- app3.py
+++ app3.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageFilter, ImageEnhance
 import os     
 from datetime import datetime  
 
-
+##깃
 def load_image(image_file) :          
     img = Image.open(image_file)
     return img
